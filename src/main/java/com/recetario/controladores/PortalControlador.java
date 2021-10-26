792505f17597fabf7486af54f3866c3eeda0c647
Axel <axelbon_10@hotmail.com>
2021-10-25 21:21:58 -0300

    Segundo Commit

--- src/main/java/com/recetario/controladores/PortalControlador.java
+++ src/main/java/com/recetario/controladores/PortalControlador.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 public class PortalControlador {
 
+    //Holiwi UwU
+    
     @GetMapping("/")
     public String index() {
         return "index.html";
