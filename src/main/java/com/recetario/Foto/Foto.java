38d90343777916dec77652c6cd58a703ed105fd5
Simon Gomez <saaimonsg2@gmail.com>
2021-10-26 08:05:24 -0300

    Foto

---
+++ src/main/java/com/recetario/Foto/Foto.java
@@ -0,0 +1,30 @@
+package com.recetario.Foto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Foto {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    private String nombre;
+
+    private String mime;
+
+    @Lob
+    @Basic(fetch = FetchType.LAZY)
+    private byte[] contenido;
+
+}
