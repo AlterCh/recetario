5cdceb546f48b5dec5b313340d9d55b57afa31e4
Axel <axelbon_10@hotmail.com>
2021-10-26 21:42:25 -0300

    Estoy haciendo el Servicio de Ingrediente(CRUD) y le puse las anotaciones a los Repository de Ingrediente y Receta

--- src/main/java/com/recetario/ingrediente/IngredienteRepository.java
+++ src/main/java/com/recetario/ingrediente/IngredienteRepository.java
@@ -1,5 +1,9 @@
 package com.recetario.ingrediente;
 
-public interface IngredienteRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IngredienteRepository extends JpaRepository<Ingrediente, String> {
     
 }
