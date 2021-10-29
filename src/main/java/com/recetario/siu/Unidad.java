da2742bac04039cb4f916acaa3044438a35c2d67
Axel <axelbon_10@hotmail.com>
2021-10-28 21:16:14 -0300

    Nuevo Paquete, Nuevas Clases de Unidad/Sistema Internacional de Unidades

---
+++ src/main/java/com/recetario/siu/Unidad.java
@@ -0,0 +1,26 @@
+package com.recetario.siu;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Data //Getter y Setter
+@ToString //To String
+@Builder //Defaults
+@AllArgsConstructor //Constructor con todos los parametros
+@NoArgsConstructor //Constructor vacío
+public class Unidad {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+}
