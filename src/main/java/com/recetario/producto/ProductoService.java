08ab6f375dc0da4c7cf0be65d8b22defa6fb74fa
Simon Gomez <saaimonsg2@gmail.com>
2021-11-03 20:17:29 -0300

    Login y producto

--- src/main/java/com/recetario/producto/ProductoService.java
+++ src/main/java/com/recetario/producto/ProductoService.java
@@ -1,19 +1,50 @@
 package com.recetario.producto;
 
+import com.recetario.errores.ErrorServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductoService {
 
     @Autowired
-    ProductoRepository productoRepository;
+    ProductoRepository repo;
 
 
     public List<Producto> getAll() {
-        List<Producto> productos = productoRepository.findAll();
+        List<Producto> productos = repo.findAll();
         return productos;
+    }
+    public void registrar(Producto producto) throws ErrorServicio {
+        try {
+            repo.save(producto);
+        }catch (Exception e){
+            throw new ErrorServicio("Error al registrar producto");
+        }
+    }
+
+    public void borrar(Producto producto) throws ErrorServicio {
+        try{
+            repo.delete(producto);
+        }catch (Exception exception){
+            throw new ErrorServicio("No se ha podido eliminar su producto");
+        }
+    }
+
+    public void modificar(Producto producto) throws ErrorServicio {
+        try{
+            Optional<Producto> aux = repo.findById(producto.getId());
+            if(aux.isPresent()){
+                Producto mProducto = producto;
+                repo.save(mProducto);
+            }else{
+                throw new Exception();
+            }
+        }catch (Exception e){
+            throw new ErrorServicio("No se ha podido modificar su producto");
+        }
     }
 }
